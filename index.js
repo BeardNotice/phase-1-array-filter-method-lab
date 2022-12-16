f2eb99a9a0783ba85646dc9ec512e6a3873a9add
Ian Flurkey <ianflurkey@gmail.com>
2022-12-16 11:45:18 -0500

    commit tree 5d53484ed0439bae0847e8ba479574ae207714e4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(source, lookingFor) {
+    return source.filter( (possibleMatch) => possibleMatch.toLowerCase() === lookingFor.toLowerCase());
+  }
+
+  function fuzzyMatch(source, testForString) {
+    return source.filter((possibleMatch)=>possibleMatch.toLowerCase().indexOf(testForString.toLowerCase()) === 0);
+  }
+
+  function matchName(source, findName) {
+    return source.filter((record)=>record.name === findName);
+  }
